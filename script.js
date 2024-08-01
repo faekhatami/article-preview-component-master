6441454a12724b711ed26608030b6cd436ebb274
faekhatami <faekhatami@gmail.com>
2024-08-01 21:01:44 +0330

    responsive links left

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.getElementById("share-button").addEventListener("click", function () {
-  document.querySelector(".share-links").classList.toggle("show");
+document
+  .getElementById("share-button")
+  .addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent click event from bubbling up to the document
+    document.querySelector(".share-links").classList.toggle("show");
+  });
+
+document
+  .getElementById("share-button-2")
+  .addEventListener("click", function (event) {
+    event.stopPropagation(); // Prevent click event from bubbling up to the document
+    document.querySelector(".share-links").classList.toggle("show");
+  });
+
+document.addEventListener("click", function (event) {
+  const shareLinks = document.querySelector(".share-links");
+  const shareButton = document.getElementById("share-button");
+  const shareButton2 = document.getElementById("share-button-2");
+
+  // Check if the click was outside the share links and the share buttons
+  if (
+    !shareLinks.contains(event.target) &&
+    !shareButton.contains(event.target) &&
+    !shareButton2.contains(event.target)
+  ) {
+    shareLinks.classList.remove("show");
+  }
 });
