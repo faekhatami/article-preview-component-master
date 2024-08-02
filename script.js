828ea2f4e11633fed653b2ac97720846b50954d4
faekhatami <faekhatami@gmail.com>
2024-08-02 16:07:53 +0330

    final code

--- script.js
+++ script.js
@@ -1,28 +1,12 @@
-document
-  .getElementById("share-button")
-  .addEventListener("click", function (event) {
-    event.stopPropagation(); // Prevent click event from bubbling up to the document
-    document.querySelector(".share-links").classList.toggle("show");
-  });
+document.addEventListener("DOMContentLoaded", () => {
+  const shareIcon = document.getElementById("share-icon");
+  const shareContainer = document.getElementById("share-container");
 
-document
-  .getElementById("share-button-2")
-  .addEventListener("click", function (event) {
-    event.stopPropagation(); // Prevent click event from bubbling up to the document
-    document.querySelector(".share-links").classList.toggle("show");
-  });
+  const handleClick = (event) => {
+    event.preventDefault();
+    shareContainer.classList.toggle("open");
+    shareIcon.classList.toggle("open");
+  };
 
-document.addEventListener("click", function (event) {
-  const shareLinks = document.querySelector(".share-links");
-  const shareButton = document.getElementById("share-button");
-  const shareButton2 = document.getElementById("share-button-2");
-
-  // Check if the click was outside the share links and the share buttons
-  if (
-    !shareLinks.contains(event.target) &&
-    !shareButton.contains(event.target) &&
-    !shareButton2.contains(event.target)
-  ) {
-    shareLinks.classList.remove("show");
-  }
+  shareIcon.addEventListener("click", handleClick);
 });
